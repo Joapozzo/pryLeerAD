2a9b900db764b913a6f54686a3db73fe506d4321
Joapozzo <pozzojoa@gmail.com>
2023-04-26 00:02:01 -0300

    Agregar archivos de proyecto.

---
+++ pryLeerAD/frmLeerAD.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace pryLeerAD
+{
+    partial class frmLeerAD
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEscribir = new System.Windows.Forms.Button();
+            this.txtEscribir = new System.Windows.Forms.TextBox();
+            this.btnLeer = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.txtBuscar = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnEscribir
+            // 
+            this.btnEscribir.Location = new System.Drawing.Point(276, 69);
+            this.btnEscribir.Name = "btnEscribir";
+            this.btnEscribir.Size = new System.Drawing.Size(176, 41);
+            this.btnEscribir.TabIndex = 0;
+            this.btnEscribir.Text = "Escribir";
+            this.btnEscribir.UseVisualStyleBackColor = true;
+            this.btnEscribir.Click += new System.EventHandler(this.btnEscribir_Click);
+            // 
+            // txtEscribir
+            // 
+            this.txtEscribir.Location = new System.Drawing.Point(66, 78);
+            this.txtEscribir.Name = "txtEscribir";
+            this.txtEscribir.Size = new System.Drawing.Size(130, 22);
+            this.txtEscribir.TabIndex = 2;
+            this.txtEscribir.TextChanged += new System.EventHandler(this.txtEscribir_TextChanged);
+            // 
+            // btnLeer
+            // 
+            this.btnLeer.Location = new System.Drawing.Point(276, 156);
+            this.btnLeer.Name = "btnLeer";
+            this.btnLeer.Size = new System.Drawing.Size(176, 41);
+            this.btnLeer.TabIndex = 1;
+            this.btnLeer.Text = "Leer";
+            this.btnLeer.UseVisualStyleBackColor = true;
+            this.btnLeer.Click += new System.EventHandler(this.btnLeer_Click);
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(276, 228);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(176, 41);
+            this.btnBuscar.TabIndex = 3;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.txtBuscar_Click);
+            // 
+            // txtBuscar
+            // 
+            this.txtBuscar.Location = new System.Drawing.Point(66, 237);
+            this.txtBuscar.Name = "txtBuscar";
+            this.txtBuscar.Size = new System.Drawing.Size(130, 22);
+            this.txtBuscar.TabIndex = 4;
+            // 
+            // frmLeerAD
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 281);
+            this.Controls.Add(this.txtBuscar);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.txtEscribir);
+            this.Controls.Add(this.btnLeer);
+            this.Controls.Add(this.btnEscribir);
+            this.Name = "frmLeerAD";
+            this.Text = "Leer Libro";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEscribir;
+        private System.Windows.Forms.TextBox txtEscribir;
+        private System.Windows.Forms.Button btnLeer;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.TextBox txtBuscar;
+    }
+}
+
---
+++ pryLeerAD/frmLeerAD.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace pryLeerAD
+{
+    public partial class frmLeerAD : Form
+    {
+        public frmLeerAD()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEscribir_Click(object sender, EventArgs e)
+        {
+
+            string texto = txtEscribir.Text;
+
+            TextWriter Escribir = new StreamWriter("Libro.txt", true);
+            Escribir.WriteLine(texto);
+            Escribir.Close();
+
+        }
+
+        private void btnLeer_Click(object sender, EventArgs e)
+        {
+            TextReader Leer = new StreamReader("Libro.txt");
+            /*MessageBox.Show(Leer.ReadLine());*/        // Leer x linea
+            MessageBox.Show(Leer.ReadToEnd());       // Lee todo hasta el final
+
+            Leer.Close();
+        }
+
+        private void txtEscribir_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtBuscar_Click(object sender, EventArgs e)
+        {
+            StreamReader Leer = new StreamReader("Libro.txt");
+            while (Leer.EndOfStream == false)
+            {
+                string aux = Leer.ReadLine();
+                if (aux.Contains(txtBuscar.Text))
+                {
+                    MessageBox.Show("Encontrado");
+                    break;
+                }
+                else
+                {
+                    MessageBox.Show("No encontrado");
+                }
+            }
+
+            Leer.Close();
+        }
+    }
+}
